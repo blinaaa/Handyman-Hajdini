ef0e972a8defc40060a9dad937a28fc0ff32846a
blinaaa <blinaavdullahu@gmail.com>
2022-04-18 00:33:01 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var modal = document.getElementById("myModal");
+var modalEle = document.querySelector(".modal");
+var modalImage = document.querySelector(".modalImage");
+Array.from(document.querySelectorAll(".ImgThumbnail")).forEach(item => {
+   item.addEventListener("click", event => {
+      modalEle.style.display = "block";
+      modalImage.src = event.target.src;
+   });
+});
+document.querySelector(".close").addEventListener("click", () => {
+   modalEle.style.display = "none";
+});
